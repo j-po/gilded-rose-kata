2ec804f5b6297e682870eb9e61f9c6ec456eee9d
Jonathan Polin <jonathan.polin@gmail.com>
2016-07-11 17:46:30 -0700

    Refactor for readability; add support for conjured items.

--- clojure/src/gilded_rose/core.clj
+++ clojure/src/gilded_rose/core.clj
@@ -2,43 +2,45 @@
 
 (defn update-quality [items]
   (map
-    (fn[item] (cond
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-        (merge item {:quality 0})
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-          (merge item {:quality (inc (inc (:quality item)))})
-          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-            (merge item {:quality (inc (inc (inc (:quality item))))})
-            (if (< (:quality item) 50)
-              (merge item {:quality (inc (:quality item))})
-              item)))
-      (< (:sell-in item) 0)
-        (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
-          (merge item {:quality 0})
-          (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-            (merge item {:quality (- (:quality item) 2)})
-            item))
-      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-        (merge item {:quality (dec (:quality item))})
-      :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+    (fn [{:keys [name quality sell-in] :as item}]
+      (update item :quality (case name
+                              "Backstage passes to a TAFKAL80ETC concert" (cond
+                                                                            (< sell-in 0) #(0)
+                                                                            (or (>= sell-in 10) (= quality 50)) identity
+                                                                            (< sell-in 5) #(+ 3 %)
+                                                                            (< sell-in 10) #(+ 2 %))
+                              "Aged Brie" (if (= quality 50)
+                                            identity
+                                            inc)
+                              "Sulfuras, Hand of Ragnaros" identity
+                              (if conjured?
+                                (if (< sell-in 0)
+                                  #(- % 4)
+                                  #(- % 2))
+                                (if (< sell-in 0)
+                                  #(- % 2)
+                                  dec)))))
+    (map (fn [item]
+           (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
+             (merge item {:sell-in (dec (:sell-in item))})
+             item))
+         items)))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
+
+(defn conjured [item]
+  (merge item {:conjured? true}))
 
 (defn update-current-inventory[]
   (let [inventory 
     [
       (item "+5 Dexterity Vest" 10 20)
+      (conjured (item "+5 Dexterity Vest" 10 20))
       (item "Aged Brie" 2 0)
       (item "Elixir of the Mongoose" 5 7)
+      (conjured (item "Elixir of the Mongoose" 5 7))
       (item "Sulfuras, Hand Of Ragnaros" 0 80)
       (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
     ]]
-    (update-quality inventory)
-    ))
+    (update-items inventory)))
